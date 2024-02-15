1b704bf863a475a42edea81346a2adfa1b8e03c2
ssohyun0 <jsh0312033@gmail.com>
2024-02-15 21:02:15 +0900

    game setting final

--- src/comp/game/IngredientStack.jsx
+++ src/comp/game/IngredientStack.jsx
@@ -2,14 +2,25 @@ import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import patty from '../../assets/img/patty.png';
 import tomato from '../../assets/img/tomato.png';
-import backicon from '../../assets/icon/backicon.svg';
+import lettuce from '../../assets/img/lettuce.png';
+import cheese from '../../assets/img/cheese.png';
+import onion from '../../assets/img/onion.png';
+import bacon from '../../assets/img/bacon.png';
+import backicon from '../../assets/img/tongs.png';
 import bread_top from '../../assets/img/bread_top.png';
 import bread_bottom from '../../assets/img/bread_bottom.png';
+import bugger from '../../assets/img/bugger.png';
+import bell from '../../assets/img/bell.png';
+import { Link } from 'react-router-dom';
 
-const IngredientStack = () => {
+const IngredientStack = ({ orders }) => {
   const [stack, setStack] = useState([]);
   const [errorMessage, setErrorMessage] = useState("");
   const [top, setTop] = useState(null);
+
+  useEffect(() => {
+    console.log("주문 배열:", orders);
+  }, [orders]);
 
   useEffect(() => {
     console.log("현재 스택 값:", stack);
@@ -22,6 +33,19 @@ const IngredientStack = () => {
     setTop(value);
     setErrorMessage("");
     console.log("스택에 값이 추가되었습니다:", value);
+
+    if (value === 0) {
+      const reversedOrders = [...orders].reverse();
+      let ordersMatch = true;
+      for (let i = 0; i < newStack.length; i++) {
+          if (newStack[i] !== reversedOrders[i]) {
+              ordersMatch = false;
+              break;
+          }
+      }
+      console.log("비교 결과:", ordersMatch ? 1 : 0);
+  }
+
   };
 
   const handlePop = () => {
@@ -52,25 +76,32 @@ const IngredientStack = () => {
     <>
       <BackButton onClick={handlePop}></BackButton>
       <Dish image={
-          top === "1" ? patty :
-          top === "2" ? tomato :
-          top === "0" ? bread_top :
-          top === "7" ? bread_bottom :
+          top === 0 ? bread_top :
+          top === 1 ? tomato :
+          top === 2 ? lettuce :
+          top === 3 ? onion :
+          top === 4 ? bacon :
+          top === 5 ? cheese :
+          top === 6 ? patty :
+          top === 7 ? bread_bottom :
           null
       }></Dish>
+      <Bell to={'/outro'}/>
       <Desk>
-        <TopIngredient onClick={() => handlePush("0")} image={bread_top}></TopIngredient>
-        <TopIngredient onClick={() => handlePush("1")} image={patty}></TopIngredient>
-        <TopIngredient onClick={() => handlePush("2")} image={tomato}></TopIngredient>
-        <TopIngredient onClick={() => handlePush("7")} image={bread_bottom}></TopIngredient>
-        <BlueCircle onClick={handleBlueCircleClick}></BlueCircle>
+        <TopIngredient onClick={() => handlePush(0)} image={bread_top}></TopIngredient>
+        <TopIngredient onClick={() => handlePush(1)} image={tomato}></TopIngredient>
+        <TopIngredient onClick={() => handlePush(2)} image={lettuce}></TopIngredient>
+        <TopIngredient onClick={() => handlePush(3)} image={onion}></TopIngredient>
+        <TopIngredient onClick={() => handlePush(4)} image={bacon}></TopIngredient>
+        <TopIngredient onClick={() => handlePush(5)} image={cheese}></TopIngredient>
+        <TopIngredient onClick={() => handlePush(6)} image={patty}></TopIngredient>
+        <TopIngredient onClick={() => handlePush(7)} image={bread_bottom}></TopIngredient>
       </Desk>
-      </>
+    </>
   );
 };
 
 export default IngredientStack;
-
 
 const TopIngredient = styled.div`
     width : 100px;
@@ -92,14 +123,6 @@ const Desk = styled.div`
     align-items: center;
 `;
 
-const BlueCircle = styled.div`
-    width: 100px;
-    height: 100px;
-    background-color: blue;
-    border-radius: 50%; /* 원형 모양으로 만듦 */
-    cursor: pointer;
-`;
-
 const BackButton = styled.div`
     left: 50px;
     top: 50px;
@@ -108,6 +131,7 @@ const BackButton = styled.div`
     height: 50px;
     cursor: pointer;
     background : url(${backicon});
+    background-size : cover;
 `;
 
 const Dish = styled.div`
@@ -119,5 +143,16 @@ const Dish = styled.div`
     background: url(${props => props.image});
     background-size: cover;
     border-radius: 50%; /* 원형 모양으로 만듦 */
+    cursor: pointer;
+`;
+
+const Bell = styled(Link)`
+    position : absolute;
+    top: 55px;
+    left: 545px;
+    width : 90px;
+    height : 90px;
+    background : url(${bell});  
+    background-size: cover;
     cursor: pointer;
 `;
